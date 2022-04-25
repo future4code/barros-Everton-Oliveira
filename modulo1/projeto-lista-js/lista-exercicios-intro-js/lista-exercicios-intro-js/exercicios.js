69000da18abe7a0e6f7144bce5d8b698d372919a
Everton Garcia de Oliveira <everton068garcia@outlook.com>
2022-04-24 23:51:47 -0300

    projeto-lista-js

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/lista-exercicios-intro-js/exercicios.js
@@ -57,67 +57,104 @@ function imprimeInformacoesUsuario() {
 }
 
 // EXERCÍCIO 05
-function imprimeTresCoresFavoritas() {
+function imprimeTresCoresFavoritas(cor1,cor2,cor3) {
   // implemente sua lógica aqui
+  cor1 = prompt('Digite sua cor favorita')
+  cor2 = prompt('Digite outra cor favorita')
+  cor3 = prompt('Digite mais uma cor favorita')
 
+  let coresFavoritas = [`${cor1}`,`${cor2}`, `${cor3}`]
+  console.log(coresFavoritas)
 }
+
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-
+  return string.toUpperCase()
 }
+string = prompt('Escreva uma frase:')
+
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
-
+  let total = custo / valorIngresso
+  return total
 }
+custo = Number(prompt("Qual o custo do espetáculo?"))
+valorIngresso = Number(prompt("Qual o valor de cada ingresso?"))
+
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
+  let tamanho = string1.length === string2.length
 
+  return tamanho
 }
+string1 = prompt("Digite uma frase ou palavra:")
+string2 = prompt("Digite outra frase ou palavra:")
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
-
+  return array[0]
 }
+
+
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
-
+  ultimo = array.length - 1
+  return array[ultimo]
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-
+  let copoVazio = array[array.length - 1]
+  array[array.length -1] = array[0]
+  array[0] = copoVazio
+  return array
 }
+let array = []
+
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
-
+  return string1.toLowerCase() === string2.toLowerCase()
 }
+string1 = prompt("Digite uma mensagem")
+string2 = prompt("Digite outra mensagem")
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
-
+  console.log(((((ano-nascimento) <= 20) && ((rg + 5) <= ano)) && ((rg + 5) >= ano)) || ((20 < (ano-nascimento) <= 50) && ((rg + 10) <= ano)) || ((ano-nascimento) > 50) && ((rg + 15) <= ano))
 }
+let ano = Number(prompt("Qual o ano atual?")) 
+let nascimento = Number(prompt("Qual seu ano de nascimento?"))
+let rg = Number(prompt("Qual ano sua carteira de identidade foi emitida?"))
+
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
-
+  return((ano % 4 === 0) && (ano % 100 !== 0)) || (ano % 400 === 0)
 }
+let ano = Number(prompt("Digite um ano:"))
+
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
+  let idade = prompt("Você tem mais de 18 anos?")
+  let ensino = prompt("Você possui ensino médio completo?")
+  let disponibilidade = prompt ("Você possui disponibilidade exclusiva durante o curso?")
 
+  let incricao = (idade === "sim") && (ensino === "sim") && (disponibilidade == "sim")
+  console.log(incricao)
 }
